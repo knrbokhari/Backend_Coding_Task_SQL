b5738595abbc7739dad984e6a48d6ee72c5a3375
kazi naeem rayhan <kazinaeemrayhan@gmail.com>
2022-12-26 09:17:58 +0600

    movies controller added

---
+++ src/controllers/moviesController.js
@@ -0,0 +1,11 @@
+import asyncHandler from '../utils/asyncHandler';
+
+export const longestDurationMovies = asyncHandler(async (req, res) => {
+  const result = await 'findLongestDurationMovies()';
+  return res.status(200).json({ data: result, success: true });
+});
+
+export const addMovie = asyncHandler(async (req, res) => {
+  const result = await 'createNewMovie(req.body)';
+  return res.status(201).json({ data: result, msg: 'Movie create successfully', success: true });
+});
--- src/routes/moviesRoutes.js
+++ src/routes/moviesRoutes.js
@@ -5,6 +5,9 @@ const router = express.Router();
 // get top 10 movies with the longest runTime
 router.get('/longest-duration-movies', 'longestDurationMovies');
 
+// create new movie
+router.post('/new-movie', 'newMovie');
+
 const configure = (app) => {
   app.use('/api/v1', router);
 };
