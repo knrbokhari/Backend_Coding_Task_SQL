5ef7261bd7663a2cdfa0dd60389f6fef9369d80a
kazi naeem rayhan <kazinaeemrayhan@gmail.com>
2022-12-27 16:04:37 +0600

    fix findTopRatedMovies query

--- src/services/movieServices.js
+++ src/services/movieServices.js
@@ -1,12 +1,16 @@
 import db from '../connectDB.js';
+const promisePool = db.promise();
+// eslint-disable-next-line import/prefer-default-
 
-// eslint-disable-next-line import/prefer-default-export
 export const getLongestDurationMovies = async () => {
   const q = `SELECT tconst, primaryTitle, runtimeMinutes, genres FROM movies
   ORDER BY runtimeMinutes DESC
   LIMIT 10;`;
-  const results = await db.query(q);
-  return results;
+  const results = await promisePool.query(`SELECT tconst, primaryTitle, runtimeMinutes, genres FROM movies
+    ORDER BY runtimeMinutes DESC
+    LIMIT 10;`);
+
+  return results[0];
 };
 
 export const createNewMovie = async (data) => {
--- src/services/ratingsServices.js
+++ src/services/ratingsServices.js
@@ -1,10 +1,12 @@
 import db from '../connectDB.js';
+const promisePool = db.promise();
 
 // eslint-disable-next-line import/prefer-default-export
 export const findTopRatedMovies = async () => {
-  const q = `SELECT tconst, primaryTitle, runtimeMinutes, genres FROM movies
-  ORDER BY runtimeMinutes DESC
-  LIMIT 10;`;
-  const results = await db.query(q);
-  return results;
+  const q = `SELECT movies.tconst, movies.primaryTitle, movies.genres, ratings.tconst, ratings.averageRating 
+  FROM movies INNER JOIN ratings ON movies.tconst = ratings.tconst 
+  WHERE averageRating > 6.0
+  ORDER BY averageRating`;
+  const results = await promisePool.query(q);
+  return results[0];
 };
